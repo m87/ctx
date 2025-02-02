c87ba86ff3333f5dbca99e508d1712f89b3db87c
m87 <bartosz.radlinski@gmail.com>
2025-02-02 18:15:30 +0100

    init

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -3,57 +3,56 @@ package ctx
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/m87/ctx/util"
+	"github.com/spf13/viper"
 )
-
-
 
 type ContextState int
 
 const (
-  ACTIVE ContextState = iota
-  FINISHED
+	ACTIVE ContextState = iota
+	FINISHED
 )
 
 type Interval struct {
-  start time.Time
-  end time.Time
-  duration int32
+	start    time.Time
+	end      time.Time
+	duration int32
 }
-
 
 type Context struct {
-  Id string
-  Description string
-  State ContextState
-  Duration int32
-  Intervals []Interval
+	Id          string
+	Description string
+	State       ContextState
+	Duration    int32
+	Intervals   []Interval
 }
-
 
 type State struct {
-  Contexts []Context
-  Current Context
+	Contexts []Context
+	Current  Context
 }
 
-
 func Load() State {
-  data, err := os.ReadFile("test/state")
-  util.Check(err, "Unable to read state file");
+	statePath := filepath.Join(viper.GetString("ctxPath"), "state")
+	data, err := os.ReadFile(statePath)
+	util.Check(err, "Unable to read state file")
 
-  state := State {}
-  err = json.Unmarshal(data, &state)
-  util.Check(err, "Unable to parse state file");
+	state := State{}
+	err = json.Unmarshal(data, &state)
+	util.Check(err, "Unable to parse state file")
 
-  return state
+	return state
 }
 
 func Save(state State) {
-  data, err := json.Marshal(state)
-  if err != nil {
-    panic(err)
-  }
-  os.WriteFile("test/state", data, 0644)
+	statePath := filepath.Join(viper.GetString("ctxPath"), "state")
+	data, err := json.Marshal(state)
+	if err != nil {
+		panic(err)
+	}
+	os.WriteFile(statePath, data, 0644)
 }
