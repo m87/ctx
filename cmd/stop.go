779353f6226b88fd4a610431ff45853d67be658a
m87 <bartosz.radlinski@gmail.com>
2025-02-06 22:50:17 +0100

    delete & stop

--- cmd/stop.go
+++ cmd/stop.go
@@ -8,9 +8,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// deleteCmd represents the delete command
-var deleteCmd = &cobra.Command{
-	Use:   "delete",
+// stopCmd represents the stop command
+var stopCmd = &cobra.Command{
+	Use:   "stop",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,28 +20,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		state := ctx.Load()
-		id := args[0]
-
-		if state.CurrentId == id {
-			state.CurrentId = ""
-		}
-
-		delete(state.Contexts, id)
-
+		ctx.Stop(&state)
 		ctx.Save(&state)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(stopCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// stopCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// stopCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- ctx/ctx.go
+++ ctx/ctx.go
@@ -19,8 +19,6 @@ const (
 	FINISHED
 )
 
-
-
 type Interval struct {
 	Start    time.Time
 	End      time.Time
@@ -52,6 +50,20 @@ func Load() State {
 	return state
 }
 
+func Stop(state *State) {
+	now := time.Now().Local()
+	if state.CurrentId != "" {
+		prev := state.Contexts[state.CurrentId]
+		interval := prev.Intervals[len(prev.Intervals)-1]
+		interval.End = now
+		interval.Duration = interval.End.Sub(interval.Start)
+		state.Contexts[state.CurrentId].Intervals[len(prev.Intervals)-1] = interval
+		prev.Duration = prev.Duration + interval.Duration
+		state.Contexts[state.CurrentId] = prev
+		state.CurrentId = ""
+	}
+}
+
 func Switch(id string, state *State, eventsRegistry *events.EventRegistry) {
 	if state.CurrentId == id {
 		return
@@ -69,13 +81,13 @@ func Switch(id string, state *State, eventsRegistry *events.EventRegistry) {
 
 	if ctx, ok := state.Contexts[id]; ok {
 		state.CurrentId = ctx.Id
-    events.Publish(events.Event{
-      DateTime: now,
-      Type: events.SWITCH_CTX,
-      Data: map[string]string{
-        "Description": ctx.Description,
-      },
-    }, eventsRegistry)
+		events.Publish(events.Event{
+			DateTime: now,
+			Type:     events.SWITCH_CTX,
+			Data: map[string]string{
+				"Description": ctx.Description,
+			},
+		}, eventsRegistry)
 		ctx.Intervals = append(state.Contexts[id].Intervals, Interval{Start: now})
 		state.Contexts[id] = ctx
 	} else {
@@ -91,5 +103,3 @@ func Save(state *State) {
 	}
 	os.WriteFile(statePath, data, 0644)
 }
-
-
