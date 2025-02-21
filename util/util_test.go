c8e0e4e008878ee413ac8d66f6c60f749f1325a5
m87 <bartosz.radlinski@gmail.com>
2025-02-22 00:21:43 +0100

    id test

--- util/util_test.go
+++ util/util_test.go
@@ -8,10 +8,33 @@ func TestGenerateShaFromDescription(t *testing.T) {
 	expected := "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
 	description := "test"
 
-	id, _ := Id(description, false)
+	id, err := Id(description, false)
 
-	if id != expected {
-		t.Fatalf("id: %s != %s", id, expected)
+	if err != nil || id != expected {
+		t.Fail()
 	}
 
+}
+
+func TestReturnErrIfEmptyDescription(t *testing.T) {
+
+	_, err := Id("", false)
+	if err == nil {
+		t.Fail()
+	}
+
+	_, err = Id(" \t", false)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestReturnIdAsIsIfIsRawFlagSet(t *testing.T) {
+	expected := "test"
+
+	id, err := Id(expected, true)
+
+	if err != nil || id != expected {
+		t.Fail()
+	}
 }
