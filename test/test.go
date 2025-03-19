092deb74966196928803f43a50979b7c9902dadf
m87 <bartosz.radlinski@gmail.com>
2025-03-20 00:36:06 +0100

    fix tests

---
+++ test/test.go
@@ -0,0 +1,7 @@
+package test
+
+var TestId string = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+var TestDescription = "test"
+
+var PrevTestId string = "27273a33c268fa97fcf0e3ddc424bd8037d7da9a08d64ff5ca4ce8c67d875897"
+var PrevDescription = "prevtest"
