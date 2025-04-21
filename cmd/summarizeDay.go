8c55559411e840cf64fde3994c588284e115def9
m87 <bartosz.radlinski@gmail.com>
2025-04-22 00:23:46 +0200

    round summary

--- cmd/summarizeDay.go
+++ cmd/summarizeDay.go
@@ -7,14 +7,36 @@ import (
 
 	"github.com/m87/ctx/ctx"
 	"github.com/m87/ctx/ctx_model"
+	"github.com/m87/ctx/util"
 	"github.com/spf13/cobra"
 )
+
+func roundDuration(d time.Duration, unit string) time.Duration {
+	switch unit {
+	case "nanosecond":
+		return d.Round(time.Nanosecond)
+	case "microsecond":
+		return d.Round(time.Microsecond)
+	case "millisecond":
+		return d.Round(time.Millisecond)
+	case "second":
+		return d.Round(time.Second)
+	case "minute":
+		return d.Round(time.Minute)
+	case "hour":
+		return d.Round(time.Hour)
+	default:
+		return d.Round(time.Nanosecond)
+	}
+}
 
 var summarizeDayCmd = &cobra.Command{
 	Use:     "day",
 	Aliases: []string{"d", "day"},
 	Short:   "Summarize day",
 	Run: func(cmd *cobra.Command, args []string) {
+		roundUnit, _ := cmd.Flags().GetString("round")
+
 		date := time.Now().Local()
 		if len(args) > 0 {
 			rawDate := strings.TrimSpace(args[0])
@@ -30,7 +52,9 @@ var summarizeDayCmd = &cobra.Command{
 
 		mgr.ContextStore.Read(func(s *ctx_model.State) error {
 			for ctxId, _ := range s.Contexts {
-				durations[ctxId], _ = mgr.GetIntervalDurationsByDate(s, ctxId, date)
+				d, err := mgr.GetIntervalDurationsByDate(s, ctxId, date)
+				util.Checkm(err, "Unable to get interval durations for context "+ctxId)
+				durations[ctxId] = roundDuration(d, roundUnit)
 			}
 			return nil
 		})
@@ -58,4 +82,5 @@ var summarizeDayCmd = &cobra.Command{
 func init() {
 	summarizeCmd.AddCommand(summarizeDayCmd)
 	summarizeDayCmd.Flags().BoolP("verbose", "v", false, "Verbose output")
+	summarizeDayCmd.Flags().StringP("round", "r", "nanosecond", "Round to the nearest nanosecond, microsecond, millisecond, second, minute, hour")
 }
