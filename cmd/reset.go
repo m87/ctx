cb0f16a9d6db422094384b004496e9b0b6ffe843
m87 <bartosz.radlinski@gmail.com>
2025-07-19 06:15:05 +0200

    ref cmds

--- cmd/reset.go
+++ cmd/reset.go
@@ -4,24 +4,29 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/m87/ctx/bootstrap"
+	"github.com/m87/ctx/core"
 	"github.com/m87/ctx/util"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var resetCmd = &cobra.Command{
-	Use:     "reset",
-	Aliases: []string{"r"},
-	Short:   "Deletes all app files",
-	Run: func(cmd *cobra.Command, args []string) {
-		home, err := os.UserHomeDir()
-		util.Checkm(err, "Unable to get user home dir")
+func NewAdmResetCmd(manager *core.ContextManager) *cobra.Command {
+	return &cobra.Command{
+		Use:     "reset",
+		Aliases: []string{"r"},
+		Short:   "Deletes all app files",
+		Run: func(cmd *cobra.Command, args []string) {
+			home, err := os.UserHomeDir()
+			util.Checkm(err, "Unable to get user home dir")
 
-		os.RemoveAll(viper.GetString("storePath"))
-		os.Remove(filepath.Join(home, ".ctx"))
-	},
+			os.RemoveAll(viper.GetString("storePath"))
+			os.Remove(filepath.Join(home, ".ctx"))
+		},
+	}
+
 }
 
 func init() {
-	admCmd.AddCommand(resetCmd)
+	admCmd.AddCommand(NewAdmResetCmd(bootstrap.CreateManager()))
 }
