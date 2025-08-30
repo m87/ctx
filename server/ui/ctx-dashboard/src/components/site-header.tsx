fb5a6a5e2bd9a058c5c9db5e70dc99b417abd031
m87 <bartosz.radlinski@gmail.com>
2025-08-30 19:14:55 +0200

    style fixes

--- server/ui/ctx-dashboard/src/components/site-header.tsx
+++ server/ui/ctx-dashboard/src/components/site-header.tsx
@@ -38,7 +38,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium flex w-full justify-start">
+        <h1 className="text-base font-medium flex w-full justify-start flex-norwap">
           <Routes>
             <Route path="/contexts" element={"Contexts"} />
             <Route path="/day/:day" element={
