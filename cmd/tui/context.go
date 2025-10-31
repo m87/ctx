18c2a4ce14bac832c3f949a786d0cfbbeee0946f
m87 <bartosz.radlinski@gmail.com>
2025-10-31 19:25:28 +0100

    comments cmd

--- cmd/tui/context.go
+++ cmd/tui/context.go
@@ -39,8 +39,19 @@ func ListFull(session core.Session) string {
 	for _, id := range ids {
 		v := state.Contexts[id]
 		output += fmt.Sprintf("- [%s] %s\n", id, v.Description)
+		output += fmt.Sprintf("  Intervals:\n")
 		for _, interval := range v.Intervals {
 			output += fmt.Sprintf("\t[%s] %s - %s\n", interval.Id, interval.Start.Time.Format(time.DateTime), interval.End.Time.Format(time.DateTime))
+		}
+
+		output += fmt.Sprintf("  Labels:\n")
+		for _, label := range v.Labels {
+			output += fmt.Sprintf("\t%s\n", label)
+		}
+
+		output += fmt.Sprintf("  Comments:\n")
+		for _, comment := range v.Comments {
+			output += fmt.Sprintf("\t[%s] %s\n", comment.Id, comment.Content)
 		}
 	}
 	return output
